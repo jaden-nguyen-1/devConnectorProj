358e8b0b1098a3570203f847ef0a4056cee9a7c8
jadenn1 <jadenn1@umbc.edu>
2021-07-28 03:15:36 -0400

    add user and profile routes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+//connect to database
+connectDB();
+
+//enable Middleware to access req.body
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req,res) => {
     res.send('HOME PAGE');
 });
+
+//define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 app.listen(PORT, (req,res) => {
     console.log(`LISTENING ON PORT ${PORT}`);
